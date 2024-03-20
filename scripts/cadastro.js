9daec6878c29dcb784abd077f8743dda36f17924
nicholascostap <nickxxtreme@gmail.com>
2024-03-20 02:29:17 -0300

    :sparkles: feat(cadastro): changes in page cadastro

--- scripts/cadastro.js
+++ scripts/cadastro.js
@@ -26,3 +26,18 @@ async function cadastroUsuario(){
 
     alert(JSON.stringify(data));
 }
+
+function formatCpf(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ 
+ }
