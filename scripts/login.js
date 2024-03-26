d9c048032722354d9a1db5b19bea3b97aef5a37b
nicholascostap <nickxxtreme@gmail.com>
2024-03-25 23:21:07 -0300

    :construct: feat(address): adjust in index, get user data in local-storage and page profile

--- scripts/login.js
+++ scripts/login.js
@@ -1,8 +1,8 @@
 const url = "https://api-go-wash-efc9c9582687.herokuapp.com/api/login"
 
 async function loginUsuario(){
-    let email = document.getElementById('email').value;
-    let password = document.getElementById('password').value;
+    let email = "henriquetgil123@gmail.com";
+    let password = "123456";
     let response = await fetch(url, {
         method: "POST",
         body: JSON.stringify({
@@ -25,6 +25,18 @@ async function loginUsuario(){
         let access_token = data.access_token
         document.getElementById("status").innerText = 'Login Realizado com Sucesso!';
         window.localStorage.setItem("access_token", access_token)
+
+        let userData = data.user;
+        
+        let values = {
+            name: userData.name,
+            email: userData.email,
+            cpfCnpj : userData.cpf_cnpj,
+            birthDate : userData.birthdate    
+        }
+    
+        window.localStorage.setItem("userData", JSON.stringify(values));
+        
     }else {
         let error_message = data.data.errors;
         document.getElementById("status").innerText = error_message + '!';
--- scripts/perfil.js
+++ scripts/perfil.js
@@ -1,19 +1,14 @@
-async function fillUserAddress(){
-    let userAddress = window.localStorage.getItem('userAddress');
-    userAddress = JSON.parse(userAddress);
+async function fillUserData(){
+    let userData = window.localStorage.getItem('userData');
+    userData = JSON.parse(userData);
     
-    let addressComplete = document.getElementById('endereco-completo');
-
-    for (i in userAddress){
-        let values = userAddress[i];
-        
-        let userAddress = {
-            title : values.title,
-            cep : values.cep,
-            address : values.address,
-            number : values.number,
-            complement : values.complement
-        }
-        addressComplete.innerText += `${userAddress.address}, ${userAddress.number} - ${userAddress.complement}`;
-    }
+    let name = document.getElementById('nome');
+    let email = document.getElementById('email');
+    let cpfCnpj = document.getElementById('cpf-cnpj');
+    let birthDate = document.getElementById('birth-date');
+    
+    name.innerText = userData.name;
+    email.innerText = userData.email;
+    cpfCnpj.innerText = userData.cpfCnpj;
+    birthDate.innerText = userData.birthDate;
 }
